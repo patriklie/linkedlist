ab5f32f8dab1b551cc31756082ac4aa9d0f8a4a3
Patrik Lie <patrik.lie@hotmail.com>
2024-08-28 16:45:40 +0200

    swap nodes

--- linkedList.mjs
+++ linkedList.mjs
@@ -110,7 +110,85 @@ class LinkedList {
         this.size--;
     }
 
+    swapNodes(node1, node2) {
+        if (!this.head) return; // empty list bare returner
+        if (node1.data === node2.data) return; // Hvis nodene er helt like så bare return trenger ikke swappe dem
 
+        // Lokalisere nodene i lista:
+
+        let currentNode1 = this.head;
+        let previousNode1 = null;
+        let currentNode2 = this.head;
+        let previousNode2 = null;
+
+        while (currentNode1 !== node1) {
+            previousNode1 = currentNode1;
+            currentNode1 = currentNode1.nextNode;
+        }
+
+        while(currentNode2 !== node2) {
+            previousNode2 = currentNode2;
+            currentNode2 = currentNode2.nextNode;
+        }
+
+        if (!currentNode1) {
+            console.log("Did not find the first node in the list!");
+            return;
+        }
+
+        if (!currentNode2) {
+            console.log("Did no find the second node in the list");
+            return;
+        }
+
+        // Her sjekker jeg om prev1 noden er null for da er den funnet men den er head i lista
+        if (!previousNode1) {
+            this.head = currentNode2; // setter head til node2;
+
+            let temp = currentNode2.nextNode
+            currentNode2.nextNode = currentNode1.nextNode; // node 2 sin neste node er node1 sin gamle neste
+
+            previousNode2.nextNode = currentNode1;
+            currentNode1.nextNode = temp;
+            return;
+        }
+
+        // Her sjekker jeg om prev2 noden er null for da er den funnet men den er head i lista
+        if(!previousNode2) {
+            this.head = currentNode1;
+
+            let temp = currentNode1.nextNode; 
+            currentNode1.nextNode = currentNode2.nextNode
+
+            previousNode1.nextNode = currentNode2;
+            currentNode2.nextNode = temp;
+            return;
+        }
+
+        let temp = currentNode1.nextNode;
+        currentNode1.nextNode = currentNode2.nextNode;
+        previousNode2.nextNode = currentNode1;
+
+        previousNode1.nextNode = currentNode2;
+        currentNode2.nextNode = temp;
+    }
+
+    findNode(data) {
+        if (!this.head) return; // empty list nothing to iterate over
+
+        let currentNode = this.head;
+
+        while (currentNode && currentNode.data !== data) {
+            currentNode = currentNode.nextNode;
+        }
+
+        if(!currentNode) {
+            console.log("Node not found!");
+            return null;
+        }
+
+        return currentNode;
+    }
 
     printList() {
         if (!this.head) {
@@ -127,11 +205,19 @@ class LinkedList {
 
 const liste1 = new LinkedList();
 
+liste1.addFirst(500);
 liste1.addFirst(400);
 liste1.addFirst(300);
 liste1.addFirst(200);
 liste1.addFirst(100);
 
-liste1.addAtIndex(500, 3);
 
+const node1 = liste1.findNode(200);
+const node2 = liste1.findNode(400);
+console.log(node1, node2);
+liste1.printList();
+
+liste1.swapNodes(node1, node2);
+
+console.log("Etter swap: ")
 liste1.printList();
